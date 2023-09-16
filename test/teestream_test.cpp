ba86bd0177d0f9852b7039fa483521817849dde1
TAKEI Yuya <takei-yuya@users.noreply.github.com>
2023-09-16 11:46:16 +0900

    add iteestream in teestream.hpp

--- test/teestream_test.cpp
+++ test/teestream_test.cpp
@@ -15,3 +15,34 @@ TEST(TeeStreamTest, testOTeeStream) {
   EXPECT("Hello World!!", ==, out1.str());
   EXPECT("Hello World!!", ==, out2.str());
 }
+
+TEST(TeeStreamTest, testITeeStream) {
+  std::istringstream iss("Hello World!!");
+  std::ostringstream oss;
+  yu::stream::iteestream its(iss, oss);
+
+  std::string str;
+  its >> str;
+  EXPECT("Hello", ==, str);
+  EXPECT("Hello World!!", ==, oss.str());  // ostream recieve whole buffer
+  its >> str;
+  EXPECT("World!!", ==, str);
+}
+
+TEST(TeeStreamTest, testITeeStreamLarge) {
+  std::vector<char> buf(1024 * 1024);
+  for (size_t i = 0; i < buf.size(); ++i) {
+    buf[i] = static_cast<char>(i % 0b11111111);
+  }
+  std::string input(buf.data(), buf.size());
+  std::istringstream iss(input);
+  std::ostringstream oss;
+  yu::stream::iteestream its(iss, oss);
+
+  std::vector<char> read_buf(buf.size());
+  its.read(read_buf.data(), read_buf.size());
+  std::string actual(read_buf.data(), its.gcount());
+
+  EXPECT(input, ==, actual);
+  EXPECT(input, ==, oss.str());
+}
--- yu/stream/teestream.hpp
+++ yu/stream/teestream.hpp
@@ -11,6 +11,7 @@ class oteestreambuf : public std::streambuf {
  public:
   oteestreambuf(std::ostream& out1, std::ostream& out2) : out1_(out1), out2_(out2) {
   }
+
  private:
   virtual std::streamsize xsputn(const char* s, std::streamsize n) {
     out1_.write(s, n);
@@ -26,6 +27,38 @@ class oteestream : public std::ostream {
  public:
   oteestream(std::ostream& out1, std::ostream& out2) : std::ostream(new oteestreambuf(out1, out2)) {}
   ~oteestream() {
+    delete rdbuf();
+  }
+};
+
+class iteestreambuf : public std::streambuf {
+ public:
+  iteestreambuf(std::istream& in, std::ostream& out) : in_(in), out_(out), buffer_(1024) {
+    setg(buffer_.data(), buffer_.data() + buffer_.size(), buffer_.data() + buffer_.size());
+  }
+
+ private:
+  virtual int underflow() {
+    if (gptr() < egptr()) return *gptr();
+
+    in_.read(buffer_.data(), buffer_.size());
+    std::streamsize size = in_.gcount();
+    if (size == 0) return traits_type::eof();
+    out_.write(buffer_.data(), size);
+    setg(buffer_.data(), buffer_.data(), buffer_.data() + size);
+    return *gptr();
+  }
+
+
+  std::istream& in_;
+  std::ostream& out_;
+  std::vector<char> buffer_;
+};
+
+class iteestream : public std::istream {
+ public:
+  iteestream(std::istream& in, std::ostream& out) : std::istream(new iteestreambuf(in, out)) {}
+  ~iteestream() {
     delete rdbuf();
   }
 };
