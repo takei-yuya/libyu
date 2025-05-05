5fbc35f2ed418b86357d41fd5b7dcb6ec8961534
TAKEI Yuya <853320+takei-yuya@users.noreply.github.com>
2025-05-05 20:07:02 +0900

    add yu::math::Rational

---
+++ test/math_rational_test.cpp
@@ -0,0 +1,394 @@
+#include "yu/math/rational.hpp"
+
+#include "yu/test.hpp"
+
+class RationalTest : public yu::Test {
+};
+
+TEST(RationalTest, testDefaultConstructor) {
+  yu::math::Rational r;
+  EXPECT(true, ==, r.is_positive());
+  EXPECT(0, ==, r.numerator());
+  EXPECT(1, ==, r.denominator());
+}
+
+TEST(RationalTest, testUnsignedConstructor) {
+  {
+    yu::math::Rational r(3u, 4u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(6u, 8u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(6u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(6u, ==, r.numerator());
+    EXPECT(1u, ==, r.denominator());
+  }
+}
+
+TEST(RationalTest, testSignedConstructor) {
+  {
+    yu::math::Rational r(3, 4);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(-3, 4);
+    EXPECT(false, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(3, -4);
+    EXPECT(false, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(-3, -4);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(-6, -8);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(6);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(6u, ==, r.numerator());
+    EXPECT(1u, ==, r.denominator());
+  }
+
+  {
+    yu::math::Rational r(-6);
+    EXPECT(false, ==, r.is_positive());
+    EXPECT(6u, ==, r.numerator());
+    EXPECT(1u, ==, r.denominator());
+  }
+}
+
+TEST(RationalTest, testTrinaryConstructor) {
+  {
+    yu::math::Rational r(true, 3u, 4u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+    EXPECT(false, ==, r.is_zero());
+    EXPECT(true, ==, r.is_valid());
+    EXPECT(false, ==, r.is_nan());
+    EXPECT(false, ==, r.is_inf());
+    EXPECT(false, ==, r.is_pos_inf());
+    EXPECT(false, ==, r.is_neg_inf());
+  }
+  {
+    yu::math::Rational r(false, 3u, 4u);
+    EXPECT(false, ==, r.is_positive());
+    EXPECT(3u, ==, r.numerator());
+    EXPECT(4u, ==, r.denominator());
+    EXPECT(false, ==, r.is_zero());
+    EXPECT(true, ==, r.is_valid());
+    EXPECT(false, ==, r.is_nan());
+    EXPECT(false, ==, r.is_inf());
+    EXPECT(false, ==, r.is_pos_inf());
+    EXPECT(false, ==, r.is_neg_inf());
+  }
+
+  {  // +0/4 is normalized to +0/1
+    yu::math::Rational r(true, 0u, 4u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(0u, ==, r.numerator());
+    EXPECT(1u, ==, r.denominator());
+    EXPECT(true, ==, r.is_zero());
+    EXPECT(true, ==, r.is_valid());
+    EXPECT(false, ==, r.is_nan());
+    EXPECT(false, ==, r.is_inf());
+    EXPECT(false, ==, r.is_pos_inf());
+    EXPECT(false, ==, r.is_neg_inf());
+  }
+  {  // -0/4 is normalized to +0/1
+    yu::math::Rational r(false, 0u, 4u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(0u, ==, r.numerator());
+    EXPECT(1u, ==, r.denominator());
+    EXPECT(true, ==, r.is_zero());
+    EXPECT(true, ==, r.is_valid());
+    EXPECT(false, ==, r.is_nan());
+    EXPECT(false, ==, r.is_inf());
+    EXPECT(false, ==, r.is_pos_inf());
+    EXPECT(false, ==, r.is_neg_inf());
+  }
+
+  {  // +0/0 is normalized to +0/0 (NaN)
+    yu::math::Rational r(true, 0u, 0u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(0u, ==, r.numerator());
+    EXPECT(0u, ==, r.denominator());
+    EXPECT(false, ==, r.is_zero());
+    EXPECT(false, ==, r.is_valid());
+    EXPECT(true, ==, r.is_nan());
+    EXPECT(false, ==, r.is_inf());
+    EXPECT(false, ==, r.is_pos_inf());
+    EXPECT(false, ==, r.is_neg_inf());
+  }
+  {  // -0/0 is normalized to +0/0 (NaN)
+    yu::math::Rational r(false, 0u, 0u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(0u, ==, r.numerator());
+    EXPECT(0u, ==, r.denominator());
+    EXPECT(false, ==, r.is_zero());
+    EXPECT(false, ==, r.is_valid());
+    EXPECT(true, ==, r.is_nan());
+    EXPECT(false, ==, r.is_inf());
+    EXPECT(false, ==, r.is_pos_inf());
+    EXPECT(false, ==, r.is_neg_inf());
+  }
+
+  {  // +4/0 is normalized to +1/0 (+Inf)
+    yu::math::Rational r(true, 4u, 0u);
+    EXPECT(true, ==, r.is_positive());
+    EXPECT(1u, ==, r.numerator());
+    EXPECT(0u, ==, r.denominator());
+    EXPECT(false, ==, r.is_zero());
+    EXPECT(false, ==, r.is_valid());
+    EXPECT(false, ==, r.is_nan());
+    EXPECT(true, ==, r.is_inf());
+    EXPECT(true, ==, r.is_pos_inf());
+    EXPECT(false, ==, r.is_neg_inf());
+  }
+  {  // -4/0 is normalized to -1/0 (-Inf)
+    yu::math::Rational r(false, 4u, 0u);
+    EXPECT(false, ==, r.is_positive());
+    EXPECT(1u, ==, r.numerator());
+    EXPECT(0u, ==, r.denominator());
+    EXPECT(false, ==, r.is_zero());
+    EXPECT(false, ==, r.is_valid());
+    EXPECT(false, ==, r.is_nan());
+    EXPECT(true, ==, r.is_inf());
+    EXPECT(false, ==, r.is_pos_inf());
+    EXPECT(true, ==, r.is_neg_inf());
+  }
+}
+
+TEST(RationalTest, testArithmetic) {
+  {
+    yu::math::Rational r1(3, 22);
+    yu::math::Rational r2(5, 77);
+
+    yu::math::Rational pos = +r1;
+    EXPECT(true, ==, pos.is_positive());
+    EXPECT(3u, ==, pos.numerator());
+    EXPECT(22u, ==, pos.denominator());
+
+    yu::math::Rational neg = -r1;
+    EXPECT(false, ==, neg.is_positive());
+    EXPECT(3u, ==, neg.numerator());
+    EXPECT(22u, ==, neg.denominator());
+
+    yu::math::Rational inv = r1.invert();
+    EXPECT(true, ==, inv.is_positive());
+    EXPECT(22u, ==, inv.numerator());
+    EXPECT(3u, ==, inv.denominator());
+
+    yu::math::Rational add = r1 + r2;
+    EXPECT(true, ==, add.is_positive());
+    EXPECT(31u, ==, add.numerator());
+    EXPECT(154u, ==, add.denominator());
+
+    yu::math::Rational sub = r1 - r2;
+    EXPECT(true, ==, sub.is_positive());
+    EXPECT(1u, ==, sub.numerator());
+    EXPECT(14u, ==, sub.denominator());
+
+    yu::math::Rational rsub = r2 - r1;
+    EXPECT(false, ==, rsub.is_positive());
+    EXPECT(1u, ==, rsub.numerator());
+    EXPECT(14u, ==, rsub.denominator());
+
+    yu::math::Rational mul = r1 * r2;
+    EXPECT(true, ==, mul.is_positive());
+    EXPECT(15u, ==, mul.numerator());
+    EXPECT(1694u, ==, mul.denominator());
+
+    yu::math::Rational div = r1 / r2;
+    EXPECT(true, ==, div.is_positive());
+    EXPECT(21u, ==, div.numerator());
+    EXPECT(10u, ==, div.denominator());
+
+    yu::math::Rational rdiv = r2 / r1;
+    EXPECT(true, ==, rdiv.is_positive());
+    EXPECT(10u, ==, rdiv.numerator());
+    EXPECT(21u, ==, rdiv.denominator());
+  }
+  {
+    yu::math::Rational r1(1, 2);
+    yu::math::Rational sub = r1 - r1;
+    EXPECT(true, ==, sub.is_positive());
+    EXPECT(0u, ==, sub.numerator());
+    EXPECT(1u, ==, sub.denominator());
+  }
+}
+
+TEST(RationalTest, testArithmeticInfNaN) {
+  yu::math::Rational zero = yu::math::Rational::Zero();
+  yu::math::Rational pos_one = yu::math::Rational(1);
+  yu::math::Rational neg_one = yu::math::Rational(-1);
+  yu::math::Rational pos_inf = yu::math::Rational::PosInf();
+  yu::math::Rational neg_inf = yu::math::Rational::NegInf();
+  yu::math::Rational nan = yu::math::Rational::NaN();
+
+  yu::math::Rational pos_two = yu::math::Rational(2);
+  yu::math::Rational neg_two = yu::math::Rational(-2);
+
+  std::vector<yu::math::Rational> rationals = {
+      zero,     pos_one,  neg_one,  pos_inf,  neg_inf,  nan,
+  };
+
+  // a + b ||    0 |    1 |   -1 | +Inf | -Inf | NaN
+  // ------++------+------+------+------+------+-----
+  // 0     ||    0 |    1 |   -1 | +Inf | -Inf | NaN
+  // 1     ||    1 |    2 |    0 | +Inf | -Inf | NaN
+  // -1    ||   -1 |    0 |   -2 | +Inf | -Inf | NaN
+  // +Inf  || +Inf | +Inf | +Inf | +Inf |  NaN | NaN
+  // -Inf  || -Inf | -Inf | -Inf |  NaN | -Inf | NaN
+  // NaN   ||  NaN |  NaN |  NaN |  NaN |  NaN | NaN
+  std::vector<std::vector<yu::math::Rational>> add_table = {
+      {zero,    pos_one,  neg_one,  pos_inf,  neg_inf,  nan},
+      {pos_one, pos_two,  zero,     pos_inf,  neg_inf,  nan},
+      {neg_one, zero,     neg_two,  pos_inf,  neg_inf,  nan},
+      {pos_inf, pos_inf,  pos_inf,  pos_inf,  nan,      nan},
+      {neg_inf, neg_inf,  neg_inf,  nan,      neg_inf,  nan},
+      {nan,     nan,      nan,      nan,      nan,      nan},
+  };
+
+  // a - b ||    0 |    1 |   -1 | +Inf | -Inf | NaN
+  // ------++------+------+------+------+------+-----
+  // 0     ||    0 |   -1 |    1 | -Inf | +Inf | NaN
+  // 1     ||    1 |    0 |    2 | -Inf | +Inf | NaN
+  // -1    ||   -1 |   -2 |    0 | -Inf | +Inf | NaN
+  // +Inf  || +Inf | +Inf | +Inf |  NaN | +Inf | NaN
+  // -Inf  || -Inf | -Inf | -Inf | -Inf |  NaN | NaN
+  // NaN   ||  NaN |  NaN |  NaN |  NaN |  NaN | NaN
+  std::vector<std::vector<yu::math::Rational>> sub_table = {
+      {zero,    neg_one,  pos_one,  neg_inf,  pos_inf,  nan},
+      {pos_one, zero,     pos_two,  neg_inf,  pos_inf,  nan},
+      {neg_one, neg_two,  zero,     neg_inf,  pos_inf,  nan},
+      {pos_inf, pos_inf,  pos_inf,  nan,      pos_inf,  nan},
+      {neg_inf, neg_inf,  neg_inf,  neg_inf,  nan,      nan},
+      {nan,     nan,      nan,      nan,      nan,      nan},
+  };
+
+  // a * b ||    0 |    1 |   -1 | +Inf | -Inf | NaN
+  // ------++------+------+------+------+------+-----
+  // 0     ||    0 |    0 |    0 |  NaN |  NaN | NaN
+  // 1     ||    0 |    1 |   -1 | +Inf | -Inf | NaN
+  // -1    ||    0 |   -1 |    1 | -Inf | +Inf | NaN
+  // +Inf  ||  NaN | +Inf | -Inf | +Inf | -Inf | NaN
+  // -Inf  ||  NaN | -Inf | +Inf | -Inf | +Inf | NaN
+  // NaN   ||  NaN |  NaN |  NaN |  NaN |  NaN | NaN
+  std::vector<std::vector<yu::math::Rational>> mul_table = {
+      {zero,    zero,     zero,     nan,      nan,      nan},
+      {zero,    pos_one,  neg_one,  pos_inf,  neg_inf,  nan},
+      {zero,    neg_one,  pos_one,  neg_inf,  pos_inf,  nan},
+      {nan,     pos_inf,  neg_inf,  pos_inf,  neg_inf,  nan},
+      {nan,     neg_inf,  pos_inf,  neg_inf,  pos_inf,  nan},
+      {nan,     nan,      nan,      nan,      nan,      nan},
+  };
+
+  // a / b ||    0 |    1 |   -1 | +Inf | -Inf | NaN
+  // ------++------+------+------+------+------+-----
+  // 0     ||  NaN |    0 |    0 |    0 |    0 | NaN
+  // 1     || +Inf |    1 |   -1 |    0 |    0 | NaN
+  // -1    || -Inf |   -1 |    1 |    0 |    0 | NaN
+  // +Inf  || +Inf | +Inf | -Inf |  NaN |  NaN | NaN
+  // -Inf  || -Inf | -Inf | +Inf |  NaN |  NaN | NaN
+  // NaN   ||  NaN |  NaN |  NaN |  NaN |  NaN | NaN
+  std::vector<std::vector<yu::math::Rational>> div_table = {
+      {nan,     zero,     zero,     zero,     zero,     nan},
+      {pos_inf, pos_one,  neg_one,  zero,     zero,     nan},
+      {neg_inf, neg_one,  pos_one,  zero,     zero,     nan},
+      {pos_inf, pos_inf,  neg_inf,  nan,      nan,      nan},
+      {neg_inf, neg_inf,  pos_inf,  nan,      nan,      nan},
+      {nan,     nan,      nan,      nan,      nan,      nan},
+  };
+
+  for (size_t i = 0; i < rationals.size(); ++i) {
+    for (size_t j = 0; j < rationals.size(); ++j) {
+      yu::math::Rational r1 = rationals[i];
+      yu::math::Rational r2 = rationals[j];
+      EXPECT(add_table[i][j], ==, r1 + r2);
+      EXPECT(sub_table[i][j], ==, r1 - r2);
+      EXPECT(mul_table[i][j], ==, r1 * r2);
+      EXPECT(div_table[i][j], ==, r1 / r2);
+    }
+  }
+
+  // negate
+  EXPECT(zero, ==, -zero);
+  EXPECT(neg_one, ==, -pos_one);
+  EXPECT(pos_one, ==, -neg_one);
+  EXPECT(neg_inf, ==, -pos_inf);
+  EXPECT(pos_inf, ==, -neg_inf);
+  EXPECT(nan, ==, -nan);
+
+  // invert
+  EXPECT(pos_inf, ==, zero.invert());
+  EXPECT(pos_one, ==, pos_one.invert());
+  EXPECT(neg_one, ==, neg_one.invert());
+  EXPECT(zero, ==, pos_inf.invert());
+  EXPECT(zero, ==, neg_inf.invert());
+  EXPECT(nan, ==, nan.invert());
+}
+
+TEST(RationalTest, testComparison) {
+  {
+    yu::math::Rational r1(3, 22);
+    yu::math::Rational r2(5, 77);
+
+    EXPECT(false, ==, r1 == r2);
+    EXPECT(true, ==, r1 != r2);
+    EXPECT(false, ==, r1 < r2);
+    EXPECT(false, ==, r1 <= r2);
+    EXPECT(true, ==, r1 > r2);
+    EXPECT(true, ==, r1 >= r2);
+
+    EXPECT(true, ==, r1 == r1);
+    EXPECT(false, ==, r1 != r1);
+    EXPECT(false, ==, r1 < r1);
+    EXPECT(true, ==, r1 <= r1);
+    EXPECT(false, ==, r1 > r1);
+    EXPECT(true, ==, r1 >= r1);
+  }
+}
+
+bool near_equal(long double a, long double b, long double epsilon = 1e-10) {
+  return std::abs(a - b) < epsilon;
+}
+
+TEST(RationalTest, testConversion) {
+  {
+    yu::math::Rational r1(-3, 2);
+    EXPECT(true, ==, near_equal(static_cast<long double>(r1), -1.5));
+    EXPECT(true, ==, near_equal(static_cast<double>(r1), -1.5));
+    EXPECT(true, ==, near_equal(static_cast<float>(r1), -1.5));
+    EXPECT(true, ==, near_equal(static_cast<float>(r1), -1.5));
+  }
+}
---
+++ yu/math/rational.hpp
@@ -0,0 +1,225 @@
+// Copyright 2025 TAKEI Yuya (https://github.com/takei-yuya/)
+#ifndef YU_MATH_RATIONAL_HPP_
+#define YU_MATH_RATIONAL_HPP_
+
+#include <type_traits>
+#include <cstdint>
+#include <iostream>
+#include <limits>
+
+#include "utils.hpp"
+
+namespace yu {
+namespace math {
+
+template <typename Uint>
+class RationalBase {
+ private:
+  template <typename T>
+  using enable_if_is_unsigned_integral_t
+    = typename std::enable_if<std::is_unsigned<T>::value && std::is_integral<T>::value, T>::type;
+
+  template <typename T>
+  using enable_if_is_signed_integral_t
+    = typename std::enable_if<std::is_signed<T>::value && std::is_integral<T>::value, T>::type;
+
+ public:
+  // Constructors
+  constexpr RationalBase() noexcept : positive_(true), numerator_(0), denominator_(1), reduced_(true) {}
+
+  template <typename T>
+  explicit RationalBase(T numerator, enable_if_is_unsigned_integral_t<T> denominator = 1) noexcept
+      : positive_(true), numerator_(numerator), denominator_(denominator), reduced_(denominator == 1) {
+    Normalize();
+  }
+
+  template <typename T>
+  explicit RationalBase(T numerator, enable_if_is_signed_integral_t<T> denominator = 1) noexcept
+      : positive_((numerator > 0) == (denominator > 0))
+      , numerator_(std::abs(numerator)), denominator_(std::abs(denominator)), reduced_(denominator == 1) {
+    Normalize();
+  }
+
+  RationalBase(bool positive, Uint numerator, Uint denominator) noexcept
+      : positive_(positive), numerator_(numerator), denominator_(denominator), reduced_(denominator == 1) {
+    Normalize();
+  }
+
+ private:
+  // Constructor for already reduced rational numbers
+  RationalBase(bool positive, Uint numerator, Uint denominator, bool reduced) noexcept
+      : positive_(positive), numerator_(numerator), denominator_(denominator), reduced_(reduced) {
+    Normalize();
+  }
+
+ public:
+  // Named constructors
+  static const RationalBase& Zero() noexcept    { static const RationalBase r(true, 0, 1, true); return r; }
+  static const RationalBase& NaN() noexcept     { static const RationalBase r(true, 0, 0, true); return r; }
+  static const RationalBase& PosInf() noexcept  { static const RationalBase r(true, 1, 0, true); return r; }
+  static const RationalBase& NegInf() noexcept  { static const RationalBase r(false, 1, 0, true); return r; }
+
+  // Accessors
+  constexpr bool is_positive() const noexcept { return positive_; }
+  constexpr Uint numerator() const noexcept { return numerator_; }
+  constexpr Uint denominator() const noexcept { return denominator_; }
+
+  constexpr bool is_zero() const noexcept { return numerator_ == 0 && denominator_ == 1; }
+  constexpr bool is_valid() const noexcept { return denominator_ != 0; }
+  constexpr bool is_nan() const noexcept { return numerator_ == 0 && denominator_ == 0; }
+  constexpr bool is_inf() const noexcept { return numerator_ != 0 && denominator_ == 0; }
+  constexpr bool is_pos_inf() const noexcept { return is_inf() && positive_; }
+  constexpr bool is_neg_inf() const noexcept { return is_inf() && !positive_; }
+
+  // Conversion operators
+  explicit operator long double() const noexcept {
+    if (is_nan()) return std::numeric_limits<long double>::quiet_NaN();
+    if (is_pos_inf()) return std::numeric_limits<long double>::infinity();
+    if (is_neg_inf()) return -std::numeric_limits<long double>::infinity();
+    return (positive_ ? 1 : -1) * static_cast<long double>(numerator_) / static_cast<long double>(denominator_);
+  }
+  explicit operator double() const noexcept { return static_cast<double>(static_cast<long double>(*this)); }
+  explicit operator float() const noexcept { return static_cast<float>(static_cast<long double>(*this)); }
+
+  // Comparison operators
+  constexpr bool operator==(const RationalBase& other) const noexcept {
+    return positive_ == other.positive_ && numerator_ == other.numerator_ && denominator_ == other.denominator_;
+  }
+  constexpr bool operator!=(const RationalBase& other) const noexcept { return !(*this == other); }
+  bool operator<(const RationalBase& other) const noexcept { return !((*this - other).is_positive()); }
+  bool operator>(const RationalBase& other) const noexcept { return other < *this; }
+  bool operator<=(const RationalBase& other) const noexcept { return !(*this > other); }
+  bool operator>=(const RationalBase& other) const noexcept { return !(*this < other); }
+
+  // Unary Arithmetic operators
+  constexpr RationalBase operator+() const noexcept { return *this; }
+  constexpr RationalBase operator-() const noexcept {
+    return RationalBase(!positive_, numerator_, denominator_, true);
+  }
+  constexpr RationalBase invert() const noexcept {
+    return RationalBase(positive_, denominator_, numerator_, true);
+  }
+
+  // Binary Arithmetic operators
+  RationalBase operator+(const RationalBase& other) const noexcept {
+    if (is_nan() || other.is_nan()) return NaN();  // NaN + x / x + NaN
+    if (is_pos_inf() && other.is_neg_inf()) return NaN();  // +Inf + -Inf
+    if (is_neg_inf() && other.is_pos_inf()) return NaN();  // -Inf + +Inf
+    if (is_pos_inf() || other.is_pos_inf()) return PosInf();  // +Inf + x / x + +Inf
+    if (is_neg_inf() || other.is_neg_inf()) return NegInf();  // -Inf + x / x + -Inf
+
+    Uint gcd = yu::math::gcd(denominator_, other.denominator_);
+    Uint n1 = numerator_ * (other.denominator_ / gcd);
+    Uint n2 = other.numerator_ * (denominator_ / gcd);
+    Uint d = denominator_ * (other.denominator_ / gcd);
+    if (is_positive() == other.is_positive()) {
+      return RationalBase(positive_, n1 + n2, d);
+    } else {
+      if (n1 > n2) {
+        return RationalBase(positive_, n1 - n2, d);
+      } else {
+        return RationalBase(!positive_, n2 - n1, d);
+      }
+    }
+  }
+  RationalBase operator-(const RationalBase& other) const noexcept {
+    return *this + (-other);
+  }
+
+  RationalBase operator*(const RationalBase& other) const noexcept {
+    if (is_nan() || other.is_nan()) return NaN();  // NaN * x / x * NaN
+    if (is_inf() && other.is_zero()) return NaN();  // +Inf/-Inf * 0
+    if (is_zero() && other.is_inf()) return NaN();  // 0 * +Inf/-Inf
+    if (is_inf()) return (other.is_positive() ? +(*this) : -(*this));  // +Inf/-Inf * x
+    if (other.is_inf()) return (is_positive() ? +other : -other);  // x * +Inf/-Inf
+
+    Uint gcd1 = yu::math::gcd(numerator_, other.denominator_);
+    Uint gcd2 = yu::math::gcd(other.numerator_, denominator_);
+    Uint n = (numerator_ / gcd1) * (other.numerator_ / gcd2);
+    Uint d = (denominator_ / gcd2) * (other.denominator_ / gcd1);
+    return RationalBase(positive_ == other.positive_, n, d, true);
+  }
+  RationalBase operator/(const RationalBase& other) const noexcept {
+    return *this * other.invert();
+  }
+
+  // Binary Arithmetic operators with integer (Rational op int)
+  template <typename T> RationalBase operator+(T other) const noexcept { return *this + RationalBase(other); }
+  template <typename T> RationalBase operator-(T other) const noexcept { return *this - RationalBase(other); }
+  template <typename T> RationalBase operator*(T other) const noexcept { return *this * RationalBase(other); }
+  template <typename T> RationalBase operator/(T other) const noexcept { return *this / RationalBase(other); }
+
+  // Binary Arithmetic operators with integer (int op Rational)
+  template <typename T> friend RationalBase operator+(T other, const RationalBase& r) noexcept { return RationalBase(other) + r; }
+  template <typename T> friend RationalBase operator-(T other, const RationalBase& r) noexcept { return RationalBase(other) - r; }
+  template <typename T> friend RationalBase operator*(T other, const RationalBase& r) noexcept { return RationalBase(other) * r; }
+  template <typename T> friend RationalBase operator/(T other, const RationalBase& r) noexcept { return RationalBase(other) / r; }
+
+  // Binary Arithmetic assignment operators
+  RationalBase& operator+=(const RationalBase& other) noexcept { *this = *this + other; return *this; }
+  RationalBase& operator-=(const RationalBase& other) noexcept { *this = *this - other; return *this; }
+  RationalBase& operator*=(const RationalBase& other) noexcept { *this = *this * other; return *this; }
+  RationalBase& operator/=(const RationalBase& other) noexcept { *this = *this / other; return *this; }
+
+  // Binary Arithmetic assignment operators with integer
+  template <typename T> RationalBase& operator+=(T other) noexcept { return *this += RationalBase(other); }
+  template <typename T> RationalBase& operator-=(T other) noexcept { return *this -= RationalBase(other); }
+  template <typename T> RationalBase& operator*=(T other) noexcept { return *this *= RationalBase(other); }
+  template <typename T> RationalBase& operator/=(T other) noexcept { return *this /= RationalBase(other); }
+
+ private:
+  void Normalize() noexcept {
+    if (denominator_ == 0) {
+      if (numerator_ == 0) {
+        positive_ = true;  // NaN
+      } else {
+        numerator_ = 1;  // +Inf/-Inf
+      }
+      return;
+    }
+    if (numerator_ == 0) { // 0 -> +0/1
+      positive_ = true;
+      denominator_ = 1;
+      return;
+    }
+    if (reduced_) return;  // already reduced
+    Uint gcd = yu::math::gcd(numerator_, denominator_);
+    numerator_ /= gcd;
+    denominator_ /= gcd;
+    reduced_ = true;
+  }
+
+  // NaN: +0/0
+  // +Inf: +1/0
+  // -Inf: -1/0
+  bool positive_;
+  Uint numerator_;
+  Uint denominator_;
+  bool reduced_;  // already reduced
+};
+using Rational = RationalBase<uint64_t>;
+
+template <typename Uint>
+std::ostream& operator<<(std::ostream& os, const RationalBase<Uint>& r) {
+  if (r.is_nan()) {
+    os << "NaN";
+  } else if (r.is_pos_inf()) {
+    os << "+Inf";
+  } else if (r.is_neg_inf()) {
+    os << "-Inf";
+  } else {
+    if (!r.is_positive()) {
+      os << "-";
+    }
+    os << r.numerator();
+    if (r.denominator() != 1) {
+      os << "/" << r.denominator();
+    }
+  }
+  return os;
+}
+
+}  // namespace math
+}  // namespace yu
+
+#endif  // YU_MATH_RATIONAL_HPP_
